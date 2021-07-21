8e85cad8c6e4461c3deb2693629ab4d00cf9a978
TaehoonLeee <lyc9290@gmail.com>
2021-07-21 16:57:00 +0900

    git account test

--- domain/src/main/java/org/kumnan/aos/apps/domain/repository/UnsplashRepository.kt
+++ domain/src/main/java/org/kumnan/aos/apps/domain/repository/UnsplashRepository.kt
@@ -7,5 +7,6 @@ import org.kumnan.aos.apps.domain.entity.status.Result
 interface UnsplashRepository {
     fun getSearchResultOfPage(query: String, page: Int): Flow<Result<List<UnsplashPhoto>>>
 
+    // PagingData를 사용하기 위한 Generic Function
     fun <T> getSearchResult(query: String): Flow<T>
 }
