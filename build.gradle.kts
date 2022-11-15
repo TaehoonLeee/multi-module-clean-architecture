c4821ad21fa925b2f0bdc422842179f2981e9a24
TaehoonLeee <lyc9290@gmail.com>
2022-11-15 16:54:18 +0900

    chore) version catalog plugins

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,9 +2,9 @@
 plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.android.library) apply false
+    alias(libs.plugins.google.service) apply false
     alias(libs.plugins.kotlin) apply false
     alias(libs.plugins.hilt) apply false
-    id("com.google.gms.google-services") version "4.3.10" apply false
 }
 
 allprojects {
